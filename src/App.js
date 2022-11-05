c073027de12a941dca7b44482985700dedfc7c43
stau04@gmail.com <stau04@gmail.com>
2022-11-06 02:22:18 +0900

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useEffect } from 'react';
 import './App.scss';
 import Home from './page/Home';
 
-axios.defaults.baseURL = 'http://localhost:8081';
-// axios.defaults.baseURL = 'http://3.37.32.130:8081';  
+// axios.defaults.baseURL = 'http://localhost:8081';
+axios.defaults.baseURL = 'http://3.37.32.130:8081';  
 axios.defaults.headers ={"Content-Type":"application/json"}
 axios.defaults.withCredentials=true;
 
