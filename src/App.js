08a65bc743b19b061967f14a01a26fec8d5f03ac
stau04@gmail.com <stau04@gmail.com>
2022-11-06 03:57:41 +0900

    cors test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.scss';
 import Home from './page/Home';
 
 // axios.defaults.baseURL = 'http://localhost:8081';
-axios.defaults.baseURL = 'http://3.37.32.130:8081';  
+// axios.defaults.baseURL = 'http://3.37.32.130:8081';  
+axios.defaults.baseURL = 'https://blog.ugosdevblog.com';  
 axios.defaults.headers ={"Content-Type":"application/json"}
 axios.defaults.withCredentials=true;
 
