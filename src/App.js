8c4088ce0e2611cc44761eedca1f8aba2295afe3
stau04@gmail.com <stau04@gmail.com>
2022-11-06 13:52:31 +0900

    cors test 4

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 import './App.scss';
 import Home from './page/Home';
 
-axios.defaults.baseURL = 'http://localhost:8081';
+// axios.defaults.baseURL = 'http://localhost:8081';
 // axios.defaults.baseURL = 'http://3.37.32.130:8081';  
-// axios.defaults.baseURL = 'https://blog.ugosdevblog.com';  
+axios.defaults.baseURL = 'https://blog.ugosdevblog.com';  
 axios.defaults.withCredentials=true;
 
 
