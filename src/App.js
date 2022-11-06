57305b37b3d623a5a17e1a23c2d66692c7cf7fe9
stau04@gmail.com <stau04@gmail.com>
2022-11-06 13:07:10 +0900

    cors test 3

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { useEffect } from 'react';
 import './App.scss';
 import Home from './page/Home';
 
-// axios.defaults.baseURL = 'http://localhost:8081';
+axios.defaults.baseURL = 'http://localhost:8081';
 // axios.defaults.baseURL = 'http://3.37.32.130:8081';  
-axios.defaults.baseURL = 'https://blog.ugosdevblog.com';  
-axios.defaults.headers['Access-Control-Allow-Origin'] = 'https://things-i-love.netlify.app';
+// axios.defaults.baseURL = 'https://blog.ugosdevblog.com';  
 axios.defaults.withCredentials=true;
 
 
