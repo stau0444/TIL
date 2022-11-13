d3fa5ba2d4b313d9d8f56cbfbc3a500cb97117c0
stau04@gmail.com <stau04@gmail.com>
2022-11-13 17:57:13 +0900

    axios baseurl 변경

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 import './App.scss';
 import Home from './page/Home';
 
- axios.defaults.baseURL = 'http://localhost:8080';
+//  axios.defaults.baseURL = 'http://localhost:8080';
 // axios.defaults.baseURL = 'http://3.37.32.130:8081';  
-// axios.defaults.baseURL = 'https://www.til-api.space:8080';  
+axios.defaults.baseURL = 'https://www.til-api.space:8080';  
 // axios.defaults.baseURL = 'http://127.0.0.1:8080';  
 axios.defaults.withCredentials=true;
 
